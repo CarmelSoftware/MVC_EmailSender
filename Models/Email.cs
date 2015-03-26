3b0850cf73c6c7300d3c492038ec040d3ed2e3f9
Carmel Shvartzman <carmel.aspnet@gmail.com>
2015-03-26 23:48:23 +0200

    Update Email.cs

--- Models/Email.cs
+++ Models/Email.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,14 @@ namespace EmailSender.Models
 {
     public class Email
     {
+        [Required]
+        [EmailAddress]
         public string To { get; set; }
         public string From { get; set; }
+        [Required]
+        [StringLength(4)]
         public string Subject { get; set; }
+        [Required]
         public string Text { get; set; }
     }
 }
